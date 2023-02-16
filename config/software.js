a4b7249f400bbe5a2b50f55899c78656d77e7406
Bridget Goodwine <65368903+bgoodwine@users.noreply.github.com>
2023-02-16 14:56:20 -0500

    revert

--- config/software.js
+++ config/software.js
@@ -40,7 +40,13 @@ function artifact( args ) {
 }
 
 const DWEETS = {
-     927: () => [ "What is this?", `<img src="https://thisartworkdoesnotexist.com/?${ performance.now() }" style="width: 10rem; max-width: 100%;">` ],
+    888: () => dweet( ( t, x ) => { // FROM: https://www.dwitter.net/d/888
+        for ( let i = 0; i < 300; i++ ) {
+            for ( let j = 0; j < 6; j++ ) {
+                x.fillRect( 100 + 66 * C( i ) * S( T( t / 1.1 ) + j / i ), 100 + 66 * S( i ), 2, 2 );
+            }
+        }
+    } ),
     1829: () => dweet( ( t, x ) => { // FROM: https://www.dwitter.net/d/1829
         for ( let i = 16; i--; ) {
             x.ellipse( 100 + 60 * S( t + i * 0.1 ), 100 + 10 * C( t + i * 0.1 ), 32 * S( -i * 0.5 ) + 32, 10 * S( i * 0.1 ) + 1, 1.6 + 0.5 * S( t * 0.5 ), 9.5, 0, true );
